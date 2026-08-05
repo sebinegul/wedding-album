4894b2b23b591d2c6d03e613a86993dc7c6b95b6
Sebi <sebi.94george@gmail.com>
2026-08-05 16:42:07 +0530

    fix

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -48,7 +48,7 @@ export function QRScanner({
     return () => {
       stopped = true;
       scanner.stop().catch(() => {});
-      scanner.clear().catch(() => {});
+      scanner.clear();
     };
   }, [onResult]);
 
